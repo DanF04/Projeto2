c90c61f8a7570c73dcb09f3824d3f8bb71682cfd
Daniel <danielmordelfer@gmail.com>
2025-05-23 16:25:11 +0100

    Created a class for the Recipe

--- MealPlanner/Model/Recipe.cs
+++ MealPlanner/Model/Recipe.cs
@@ -4,11 +4,27 @@ using System.Collections.Generic;
 namespace MealPlanner.Model
 {
     /// <summary>
-    /// Implementation of ICook. 
+    /// Implementation of Recipe. 
     /// </summary>
-    public class Recipe : Recipe
+   public class Recipe : IRecipe
     {
+
+        public string Name { get; }
+        public IReadOnlyDictionary<IIngredient, int> RequiredIngredient { get; }
+        public double SuccessRate { get; }
         
+        public Recipe(string name, double successRate, Dictionary<IIngredient, int> ingredient)
+        {
+            Name = name;
+            SuccessRate = successRate;
+            RequiredIngredients = ingredients;
+        }
+        
+        public int CompareTo(IRecipe obj)
+        {
+            if (obj == null) return 1;
+            return Name.CompareTo(obj.Name);
+        }
         
     }
 }
