c69ecd461dd103c481de3f7071b2b0d7b2ba22ec
Daniel <danielmordelfer@gmail.com>
2025-05-23 16:19:07 +0100

    created two new class files

---
+++ MealPlanner/Model/Ingredient.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace MealPlanner.Model
+{
+    /// <summary>
+    /// Implementation of ICook. 
+    /// </summary>
+    public class Ingredient : IComparable<>
+    {
+        
+    }
+}
---
+++ MealPlanner/Model/Recipe.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace MealPlanner.Model
+{
+    /// <summary>
+    /// Implementation of ICook. 
+    /// </summary>
+    public class Recipe : Recipe
+    {
+        
+        
+    }
+}
