f168e50a917518121453e148c93eaa3e92cefe85
Daniel <danielmordelfer@gmail.com>
2025-05-23 16:32:35 +0100

    Created an Ingredient Class

--- MealPlanner/Model/Ingredient.cs
+++ MealPlanner/Model/Ingredient.cs
@@ -4,10 +4,25 @@ using System.Collections.Generic;
 namespace MealPlanner.Model
 {
     /// <summary>
-    /// Implementation of ICook. 
+    /// Implementation of Ingredient. 
     /// </summary>
-    public class Ingredient : IComparable<>
+    public class Ingredient : IIngredient
     {
         
+        public string Type { get; }
+        public string Name { get; }
+
+        public Ingredient(string type, string name)
+        {
+            Type = type;
+            Name = name;
+        }
+
+        public bool Equals(IIngredient obj)
+        {
+            if (obj == null || obj.GetType() != GetType()) return false;
+            Ingredient other = (Ingredient) obj;
+            return Type == other.Type || Name == other.Name;
+        }
     }
 }
